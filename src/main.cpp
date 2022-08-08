cf158c88feca9f20706d82f8c718ae791e9b30c3
Ruben <cyclops-github@prof-x.net>
2022-08-08 20:46:52 +0100

    Move to GNSS lib

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <Wire.h>
 #include <vl53l0x_class.h>
-#include <SparkFun_Ublox_Arduino_Library.h>
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
 #include "batteryhelper.h"
 #include "lorahelper.h"
 #include "ledhelper.h"
@@ -13,13 +13,13 @@
 #include "rtos.h"
 #endif
 
-//#define MAX_SAVE
+#define MAX_SAVE
 #define SLEEPTIME (1000 * 300)
 
 SemaphoreHandle_t g_taskEvent = NULL;
 SoftwareTimer g_taskWakeupTimer;
 VL53L0X g_vl53l0x(&Wire, WB_IO2);
-SFE_UBLOX_GPS g_GPS;
+SFE_UBLOX_GNSS g_GNSS;
 uint16_t g_msgcount = 0;
 
 void periodicWakeup(TimerHandle_t unused)
@@ -71,8 +71,6 @@ void setup()
       break;
     }
   }
-
-  delay(3000); // just wait 3 sec so we can plugin the connection or setup the serial session.
 #endif
 
   // Setup/start the wire that we use for the Sensor.
@@ -85,7 +83,7 @@ void setup()
   digitalWrite(WB_IO2, HIGH);
   delay(1000);
 
-  if (g_GPS.begin() == false)
+  if (g_GNSS.begin() == false)
   {
 #ifndef MAX_SAVE
     Serial.println("Ublox GPS not detected at default I2C address. Please check wiring. Halting.");
@@ -94,13 +92,14 @@ void setup()
   }
 
   // Setup the GPS.
-  g_GPS.setDynamicModel(DYN_MODEL_WRIST);
-  g_GPS.setI2COutput(COM_TYPE_UBX);
+  g_GNSS.setDynamicModel(DYN_MODEL_WRIST);
+  g_GNSS.setI2COutput(COM_TYPE_UBX);
+  g_GNSS.saveConfiguration();
 
   LoraHelper::InitAndJoin();
 
   // Go into sleep mode
-  g_GPS.powerOff(SLEEPTIME);
+  g_GNSS.powerOff(SLEEPTIME);
   g_taskEvent = xSemaphoreCreateBinary();
   xSemaphoreGive(g_taskEvent);
   g_taskWakeupTimer.begin(SLEEPTIME, periodicWakeup);
@@ -115,9 +114,10 @@ void loop()
 
     uint32_t gpsStart = millis();
     byte gpsFixType = 0;
+    digitalWrite(VAL_RXM_PMREQ_WAKEUPSOURCE_EXTINT0, HIGH);
     while (gpsFixType < 3 && (millis() - gpsStart < (1000 * 90)))
     {
-      gpsFixType = g_GPS.getFixType(); // Get the fix type
+      gpsFixType = g_GNSS.getFixType(); // Get the fix type
 #ifndef MAX_SAVE
       Serial.print(F("Fix: ")); // Print it
       Serial.print(gpsFixType);
@@ -138,11 +138,11 @@ void loop()
       LedHelper::BlinkDelay(LED_BLUE, 500);
     }
     uint16_t gpsTime = millis() - gpsStart;
-    uint32_t gpsLat = g_GPS.getLatitude();
-    uint32_t gpsLong = g_GPS.getLongitude();
-    uint8_t gpsSats = g_GPS.getSIV();
+    uint32_t gpsLat = g_GNSS.getLatitude();
+    uint32_t gpsLong = g_GNSS.getLongitude();
+    uint8_t gpsSats = g_GNSS.getSIV();
 
-    g_GPS.powerOff(SLEEPTIME);
+    g_GNSS.powerOff(SLEEPTIME);
 #ifndef MAX_SAVE
     Serial.printf("GPS details: GPStime: %dms; SATS: %d; FIXTYPE: %d; LAT: %d; LONG: %d;\r\n", gpsTime, gpsSats, gpsFixType, gpsLat, gpsLong);
 #endif
